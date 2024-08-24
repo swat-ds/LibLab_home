4fa570f549325228f9c20d6ae72f6fd4a4d49bc7
afayyaz1 <afayyaz1@swarthmore.edu>
2024-08-24 09:48:31 -0400

    chore: Update framer-motion to version 11.3.30

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,104 @@
 import './App.css'
 import Line from './components/Line.jsx'
+import { motion } from 'framer-motion'
 
 function App() {
+  const fadeInAnimation = {
+    opacity: [0, 1],
+    x: [50, 0]
+  };
+
+  const transitionSettings = {
+    duration: 1,
+    ease: 'easeOut'
+  };
+
   return (
     <div className="flex flex-col w-screen h-screen bg-[#1A0000] overflow-hidden">
       {/* Header Section */}
-      <div className="flex justify-start h-[15vh] md:h-[20vh] w-screen pl-4 md:pl-12">
+      <motion.div
+        className="flex justify-start h-[15vh] md:h-[20vh] w-screen pl-4 md:pl-12"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 0 }}
+      >
         <h1 className="text-[#A11833] content-center text-xl sm:text-2xl md:text-4xl lg:text-5xl whitespace-nowrap">
           <span className="font-semibold">LIBLAB </span>
           <span className="font-light">SWARTHMORE</span>
         </h1>
-      </div>
+      </motion.div>
 
       {/* Main Content */}
-      <div className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12">
+      <motion.div
+        className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 0.25 }}
+      >
         <a href='https://swat-ds.github.io/liblab/' target="_blank" rel="noopener noreferrer">
-          <h1 className="content-start text-2xl sm:text-4xl md:text-7xl lg:text-8xl whitespace-nowrap">
+          <h1 className="content-start text-4xl sm:text-5xl md:text-7xl lg:text-8xl whitespace-nowrap">
             <span className="font-medium">ds@</span>
             <span className="font-extrabold">SWAT </span>
             <span className="font-medium">syllabus</span>
           </h1>
         </a>
         <Line width="40%" thickness="2px" color="#A11833" />
-      </div>
+      </motion.div>
 
       {/* Sign-Up Form */}
-      <div className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12">
+      <motion.div
+        className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 0.5 }}
+      >
         <a href='https://docs.google.com/forms/d/e/1FAIpQLSe8ULGOedRW8gNlLjGMoWDBoDe6DhL6kC665gcZLhUG7MB2ug/viewform?usp=sf_link' target="_blank" rel="noopener noreferrer">
-          <h1 className="content-start text-2xl sm:text-4xl md:text-7xl lg:text-8xl whitespace-nowrap">sign-up form</h1>
+          <h1 className="content-start text-4xl sm:text-5xl md:text-7xl lg:text-8xl whitespace-nowrap">sign-up form</h1>
         </a>
         <Line width="40%" thickness="2px" color="#A11833" />
-      </div>
+      </motion.div>
 
       {/* Immersive Realities */}
-      <div className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12">
+      <motion.div
+        className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 0.75 }}
+      >
         <a href='https://ahmad-fayyaz.github.io/LibLabMain/' target="_blank" rel="noopener noreferrer">
-          <h1 className="content-start text-2xl sm:text-4xl md:text-7xl lg:text-8xl whitespace-nowrap">immersive realities</h1>
+          <h1 className="content-start text-4xl sm:text-5xl md:text-7xl lg:text-8xl whitespace-nowrap">immersive realities</h1>
         </a>
         <Line width="40%" thickness="2px" color="#A11833" />
-      </div>
+      </motion.div>
 
       {/* Docs Section */}
-      <div className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12">
+      <motion.div
+        className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 1 }}
+      >
         <a href='https://swat-ds.github.io/docsLibLab/' target="_blank" rel="noopener noreferrer">
-          <h1 className="content-start text-2xl sm:text-4xl md:text-7xl lg:text-8xl whitespace-nowrap">docs.</h1>
+          <h1 className="content-start text-4xl sm:text-5xl md:text-7xl lg:text-8xl whitespace-nowrap">docs.</h1>
         </a>
         <Line width="40%" thickness="2px" color="#A11833" />
-      </div>
+      </motion.div>
 
       {/* GitHub Section */}
-      <div className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12">
-        <h1 className="content-start text-2xl sm:text-4xl md:text-7xl lg:text-8xl whitespace-nowrap">github.</h1>
+      <motion.div
+        className="flex flex-col justify-end h-[10vh] md:h-[12vh] w-screen pl-4 md:pl-12"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 1.25 }}
+      >
+        <h1 className="content-start text-4xl sm:text-5xl md:text-7xl lg:text-8xl whitespace-nowrap">github.</h1>
         <Line width="40%" thickness="2px" color="#A11833" />
-      </div>
+      </motion.div>
 
       {/* Footer Section */}
-      <div className="flex flex-col justify-end items-end h-[15vh] md:h-[20vh] w-screen pr-4 md:pr-12 pb-3">
+      <motion.div
+        className="flex flex-col justify-end items-end h-[15vh] md:h-[20vh] w-screen pr-4 md:pr-12 pb-3"
+        animate={fadeInAnimation}
+        transition={{ ...transitionSettings, delay: 1.5 }}
+      >
         <a href='https://creativecommons.org/licenses/by-nc/4.0/' target="_blank" rel="noopener noreferrer">
           <h6 className="text-xs sm:text-sm md:text-base text-[#A11833]">ALL RIGHTS RESERVED © LIBLAB 2024</h6>
         </a>
-      </div>
+      </motion.div>
     </div>
   );
 }
