5d03b786538dceacca43aea28d5d7af59207daf4
Roberto Vargas <betovargas@users.noreply.github.com>
2024-08-27 13:20:07 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LibLab_home/',
+  base: 'http://liblab.swarthmore.edu/',
+  
 })
