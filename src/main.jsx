79e3a63669a6169bbe8d7b7c8c53ac2f21024a87
betovargas <rvargas1@swarthmore.edu>
2024-08-29 11:36:08 -0400

    updates cursor

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App.jsx'
 import { CustomCursor } from './components/CustomCursor/index.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <CustomCursor />
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+ // <StrictMode>
+ //   <CustomCursor />
+ //   <App />
+ // </StrictMode>,
+ // )
